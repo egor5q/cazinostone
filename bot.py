d4dda5efbb5cff8724d89d8f9738f7c74ab614d0
egor5q <31898567+egor5q@users.noreply.github.com>
2019-01-20 17:30:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,10 +14,53 @@ bot = telebot.TeleBot(token)
 
 
 client=MongoClient(os.environ['database'])
-db=client.
+db=client.cazinostone
 users=db.users
 
+games={}
 
+effects=['silence','buff']
+types=['deathrattle','battlecry','special']
+
+
+@bot.message_handler(commands=['cazino'])
+def cazino(m):
+    games.update(creategame(m.chat.id))
+    
+
+
+
+
+def createeffect(x):
+    targets=['enemy','ally']
+    silence={
+        'name':'silence',
+        'target':random.choice(targets)
+    }
+    buff={
+        'name':'buff',
+        'target':'ally'
+    }
+    if x=='silence':
+        return silence
+    elif x=='buff':
+        return buff
+
+    
+def createcard(x):
+    return {
+        'type':x,
+        'battlecries':[],
+        'deathrattles':[]
+    }
+
+def creategame(id):
+    return {
+        'players':[],
+        'turn':1
+    }
+    
+    
 
 if True:
    print('7777')
